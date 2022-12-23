8d5cec9c1e4a9c9323d49ee52ef5ef0da4b9f63c
karemanyasser <kareman.elsalam01@eng-st.cu.edu.eg>
2022-12-23 11:31:18 +0200

    draw rec

--- java.js
+++ java.js
@@ -1,28 +1,12 @@
-// let width = document.querySelector('width')
-// let Height = document.querySelector('Height')
-// let yaxis= document.querySelector('yaxis')
-// let xaxis= document.querySelector('xaxis')
-// let DrawRec = document.querySelector('.DrawRec')
-function getInputValue(){
-    var width = document.getElementById("width").value.indexOf("@");
+function drawrec(){
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d"); 
+    var width = document.getElementById("width").value;
     var Height = document.getElementById("Height").value;
     var yaxis= document.getElementById("yaxis").value;
     var xaxis= document.getElementById("xaxis").value;
-
-return width, Height,yaxis,xaxis}
-
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-// function DrawRectan(){
-//     ctx.beginPath();
-//     ctx.rect(xaxis, yaxis, width, Height);
-//     ctx.stroke();
-// }
-ctx.beginPath();
-ctx.rect(xaxis, yaxis, width, Height);
-ctx.stroke();
-
-
-// DrawRec.onclick = function(){
-//     DrawRectan()
-// }
+    this.ctx.beginPath();
+    this.ctx.rect(xaxis, yaxis, width, Height);
+    this.ctx.stroke();
+    
+}
